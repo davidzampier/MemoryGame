02aa5443c4382d8b6603293c493164dcb7aa5a1a
David Zampier <d.zampier@cinq.com.br>
2020-05-19 21:14:28 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by David Zampier on 19/05/20.
+//  Copyright © 2020 David Zampier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
